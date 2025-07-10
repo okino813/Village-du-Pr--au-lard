eddb3a35ca3a466ebe54cc8fc9b106dd768fd429
okino813 <ixemgod@gmail.com>
2025-07-10 15:06:51 +0200

    First Commit

---
+++ database/migrations/4_events_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('event_title');
+            $table->string('event_content');
+            $table->string('event_img_preview');
+            $table->string('event_slug')->unique();
+            $table->foreignId('id_place')->constrained('places')->onDelete('cascade');
+            $table->foreignId('id_user')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
